aad887ad415e59cb967d63e74f6809cff0f5d4f4
Eric Baranowski <eric@kulado.com>
2022-07-04 00:39:26 -0400

    init

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer class="footer">
+	<div class="container footer-inner">
+		<div class="text-row">
+			<p>
+				<strong>
+					<?php bloginfo('name'); ?>
+				</strong>
+			</p>
+			<p class="text-gray-400">
+				Copyright <?php echo date('Y'); ?> &copy; All rights reserved
+			</p>
+		</div>
+	</div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ js/footer-bundle.js
@@ -0,0 +1,1 @@
+"use strict";$(window).on("load",function(){console.log("Loaded!")});
